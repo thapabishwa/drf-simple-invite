d683cb3619e95180897575a5f6b26263fca0bff5
Bishwa Thapa <thapabishwakranti@gmail.com>
2019-01-30 01:51:44 +0100

    Initiated a working code base, added migrations

--- drf_simple_invite/urls.py
+++ drf_simple_invite/urls.py
@@ -1,9 +1,13 @@
-from django.conf.urls import url
-
+from django.conf.urls import url,include
+from django.urls import path
+from rest_framework import routers
 from drf_simple_invite.views import InvitationTokenViewSet
 
-app_name = 'invitation_token'
+router = routers.DefaultRouter()
 
+
+router.register(r'(?P<invitation_token>.+)', InvitationTokenViewSet, base_name='invitation_token_consume')
+app_name='drf_simple_invite'
 urlpatterns = [
-    url(r'^(?P<invitation_token>.+)/', InvitationTokenViewSet, name="reset-password-confirm"),
+    path(r'', include(router.urls)),
 ]
--- drf_simple_invite/views.py
+++ drf_simple_invite/views.py
@@ -8,18 +8,18 @@ from drf_simple_invite.models import InvitationToken
 from drf_simple_invite.serializers import PasswordSerializer
 
 
-class InvitationTokenViewSet(mixins.CreateModelMixin, viewsets.ModelViewSet):
+class InvitationTokenViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = (AllowAny,)
-    serializer_classes = PasswordSerializer
+    serializer_class = PasswordSerializer
     renderer_classes = (renderers.JSONRenderer, renderers.BrowsableAPIRenderer)
 
-    def create(self):
-        if 'invitation_token' in self.kwargs:
+    def create(self, request,*args, **kwargs ):
+
+        if 'invitation_token' in kwargs:
             invitation_token = get_object_or_404(InvitationToken, id=self.kwargs['invitation_token'])
-
             password = self.request.data['password']
-
             invitation_token.user.set_password(password)
-
+            invitation_token.user.save()
+            InvitationToken.objects.filter(user=invitation_token.user).delete()
             return Response({'detail': 'Password sucessfully created'}, status=status.HTTP_201_CREATED)
         return Response({'detail': 'Cannot Find Invitation Token'}, status=status.HTTP_204_NO_CONTENT)
