7e90bc34b5fbb5c433832bcc5179af82e8bdceae
Bishwa Kranti Thapa <thapabishwakranti@gmail.com>
2019-04-01 15:52:19 +0200

    Strengthen Password Validation and Introduce InviteViewSet

--- drf_simple_invite/serializers.py
+++ drf_simple_invite/serializers.py
@@ -4,3 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class PasswordSerializer(serializers.Serializer):
     password = serializers.CharField(label=_("Password"), style={'input_type': 'password'})
+
+
+class EmailSerializer(serializers.Serializer):
+    email = serializers.EmailField(allow_blank=False, allow_null=False)
--- drf_simple_invite/views.py
+++ drf_simple_invite/views.py
@@ -1,19 +1,20 @@
 import base64
 
+from django.conf import settings
+from django.contrib.auth.password_validation import validate_password, get_password_validators
+from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
-from rest_framework import renderers
-from rest_framework import viewsets, mixins, status
-from rest_framework.permissions import AllowAny
+from rest_framework import viewsets, mixins, status, serializers
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from drf_simple_invite.models import InvitationToken
-from drf_simple_invite.serializers import PasswordSerializer
-
+from drf_simple_invite.serializers import PasswordSerializer, EmailSerializer
+from .models import AUTH_USER_MODEL
 
 class InvitationTokenViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = (AllowAny,)
     serializer_class = PasswordSerializer
-    renderer_classes = (renderers.JSONRenderer, renderers.BrowsableAPIRenderer)
 
     def create(self, request,*args, **kwargs ):
         try:
@@ -22,10 +23,36 @@ class InvitationTokenViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
                 decoded_token = base64.url_sage_b64decode(invitation_token.encode()).decode()
                 invitation_token = get_object_or_404(InvitationToken, id=decoded_token)
                 password = self.request.data['password']
-                invitation_token.user.set_password(password)
-                invitation_token.user.save()
-                InvitationToken.objects.filter(user=invitation_token.user).delete()
-                return Response({'detail': 'Password sucessfully created.'}, status=status.HTTP_201_CREATED)
+
+                try:
+                    validate_password(password, user=invitation_token.user, password_validators=get_password_validators(settings.AUTH_PASSWORD_VALIDATORS))
+                    invitation_token.user.set_password(password)
+                    invitation_token.user.save()
+                    InvitationToken.objects.filter(user=invitation_token.user).delete()
+                    return Response({'detail': 'Password sucessfully created.'}, status=status.HTTP_201_CREATED)
+                except ValidationError as e:
+                    raise serializers.ValidationError(e.messages)
+
             return Response({'detail': 'Cannot Find Invitation Token in the url.'}, status=status.HTTP_204_NO_CONTENT)
+        except:
+            pass
+
+
+class InviteViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    permission_classes = (IsAuthenticated)
+    serializer_class = EmailSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        email = request.data['email']
+        try:
+            users = AUTH_USER_MODEL.objects.get(email__iexact=email)
+        except:
+            pass
+        try:
+            InvitationToken.objects.create(users=users)
+            return Response({'detail': 'Invite User'})
         except:
             pass
