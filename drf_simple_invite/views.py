8675b10d89b7a0861393274e3a041b67d264347c
Bishwa Thapa <thapabishwakranti@gmail.com>
2022-09-16 20:07:58 +0545

    add changes required for django 4.0
    - import `gettext_lazy` instead of `ugettext_lazy`
    - import `django.urls` instead of `django.conf.urls`
    - update signals
    154d14

--- drf_simple_invite/views.py
+++ drf_simple_invite/views.py
@@ -22,11 +22,11 @@ class SetUserPasswordView(generics.CreateAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         try:
-            invitation_token = request.data['invitation_token']
-            decoded_token = base64.urlsafe_b64decode(invitation_token.encode()).decode()
-            invitation_token = get_object_or_404(InvitationToken, id=decoded_token)
+            encoded_invitation_token = request.data['invitation_token']
+            decoded_invitation_token = base64.urlsafe_b64decode(encoded_invitation_token.encode()).decode()
+            invitation_token = get_object_or_404(InvitationToken, id=decoded_invitation_token)
         except Exception as e:
-            raise serializers.ValidationError({'detail': 'Invalid Invitation Token'})
+            raise serializers.ValidationError({'detail': 'Invalid Invitation Token', 'error': e.__str__()})
 
         try:
             password = request.data['password']
